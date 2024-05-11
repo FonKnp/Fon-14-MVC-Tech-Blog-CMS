3583f63476a165b54421a58f4ae7747147327cd6
FonKnp <fonnfonk@gmail.com>
2024-05-11 14:09:32 +0200

    edit

--- models/Comment.js
+++ models/Comment.js
@@ -15,7 +15,7 @@ Comment.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
-    created_at: {
+    date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
