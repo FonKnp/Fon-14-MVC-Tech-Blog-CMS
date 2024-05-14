f20db7e262ae80a1a0de7d9206480a23826c6538
FonKnp <fonnfonk@gmail.com>
2024-05-14 09:31:25 -0400

    post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,8 +4,19 @@ const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll();
-    res.status(200).json(postData);
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username', 'id'],
+        }
+      ]
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('homepage', { 
+      posts, 
+      loggedIn: req.session.logged_in 
+    });
   } catch (err) {
     res.status(500).json(err);
   }
@@ -13,29 +24,35 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const postData = await Post.findByPk(req.params.id);
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['username','id'],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['username','id'],
+            },
+          ],
+        },
+      ],
+    }
+    );
     if (!postData) {
       res.status(404).json({ message: 'No post found with this id!' });
       return;
     }
-    res.status(200).json(postData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+    const post = postData.get({ plain: true });
 
-router.get('/user/:id', async (req, res) => {
-  try {
-    const postData = await BlogPost.findAll({
-      where: {
-        user_id: req.params.id,
-      },
+    res.render('singlePost', { 
+      post, 
+      logged_in: req.session.logged_in ,
+      currentUserID:req.session.userId
     });
-    if (!postData) {
-      res.status(404).json({ message: 'No post found with this id!' });
-      return;
-    }
-    res.status(200).json(postData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,28 +61,36 @@ router.get('/user/:id', async (req, res) => {
 router.post('/', withAuth, async (req, res) => {
   try {
     const newPost = await Post.create({
-      ...req.body,
+      title: req.body.title,
+      description: req.body.description,
       user_id: req.session.user_id,
     });
-
-    res.status(200).json(newPost);
+    res.status(200).redirect(`/`);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.put('/:id', auth, async (req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
   try {
-    const postData = await Post.update(req.body, {
-      where: {
-        id: req.params.id,
+    const [updatedPost] = await Post.update(
+      {
+        title: req.body.title,
+        description: req.body.description,
       },
-    });
-    if (!postData) {
-      res.status(404).json({ message: 'No blog post found with this id!' });
+      {
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      }
+    );
+    if (!updatedPost) {
+      res.status(404).render('404',{ message: 'No post found with this id!'})
       return;
     }
-    res.status(200).json(postData);
+
+    res.status(200).json({message:"Successfully updated"});
   } catch (err) {
     res.status(500).json(err);
   }
