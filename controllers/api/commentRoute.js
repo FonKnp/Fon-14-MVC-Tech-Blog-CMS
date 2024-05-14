ac204b17d51f57034ae4f2c1ac5cda008f100c7d
FonKnp <fonnfonk@gmail.com>
2024-05-14 09:13:46 -0400

    comment rout

--- controllers/api/commentRoute.js
+++ controllers/api/commentRoute.js
@@ -2,33 +2,44 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+
+router.get('/', async (req, res) => {
+  try {
+    const dbCommentData = await Comment.findAll({});
+    res.json(dbCommentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newComment = await Comment.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
-
-    res.status(200).json(newComment);
+    if (req.session) {
+      const newComment = await Comment.create({
+        comment: req.body.comment,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id,
+      });
+      res.redirect(`/posts/${req.body.post_id}`);
+    }
   } catch (err) {
+    console.log(err);
     res.status(400).json(err);
   }
 });
 
-router.delete('/:id', auth, async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const commentData = await Comments.destroy({
+    const commentData = await Comment.destroy({
       where: {
         id: req.params.id,
-        user_id: req.session.user_id,
       },
     });
-
     if (!commentData) {
       res.status(404).json({ message: 'No comment found with this id!' });
       return;
     }
-
     res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
@@ -37,18 +48,16 @@ router.delete('/:id', auth, async (req, res) => {
 
 router.put('/:id', auth, async (req, res) => {
   try {
-    const commentData = await Comments.update(req.body, {
+    const commentData = await Comment.update(req.body, {
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
       },
     });
-
     if (!commentData) {
       res.status(404).json({ message: 'No comment found with this id!' });
       return;
     }
-
     res.status(200).json(commentData);
   } catch (err) {
     res.status(500).json(err);
